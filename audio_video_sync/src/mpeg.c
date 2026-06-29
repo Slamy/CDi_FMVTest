668da7e8fd106e0e8e26cc4040be21ef193b0980
Andre Zeps <andre.zeps@googlemail.com>
2026-06-29 16:53:54 +0200

    uncover featuring tatjana

--- audio_video_sync/src/mpeg.c
+++ audio_video_sync/src/mpeg.c
@@ -283,11 +283,11 @@ void playMpeg() {
         printf("Serial stub?\n");
         /* mpegFile = open("/cd/MPEGAV/AVSEQ01.DAT", _READ); */
         /* mpegFile = open("/cd/MPEGAV/MUSIC01.DAT", _READ); */ /* Top Gun*/
-        mpegFile = open("/cd/seq2.rtf", _READ); /* Addams Family Disc 2 */
+        mpegFile = open("/cd/STILLS/t01", _READ); /* Addams Family Disc 2 */
     }
     DEBUG(mpegFile >= 0);
 
-    DEBUG(lseek(mpegFile, 0x8d65800, 0)); /* Mamushka! */
+    DEBUG(lseek(mpegFile, 0, 0));
     DEBUG(ss_play(mpegFile, &mpegPcb));
     printf("Started Play %d\n", mpegFile);
 #endif
