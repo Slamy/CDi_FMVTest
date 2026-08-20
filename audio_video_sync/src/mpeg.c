e2c469a74665b2fd786aeb4af4ccc203f68fbe6c
Andre Zeps <andre.zeps@googlemail.com>
2026-08-20 20:42:20 +0200

    back to lost ride ma

--- audio_video_sync/src/mpeg.c
+++ audio_video_sync/src/mpeg.c
@@ -280,12 +280,11 @@ void playMpeg() {
         printf("Serial stub?\n");
         /* mpegFile = open("/cd/MPEGAV/AVSEQ01.DAT", _READ); */
         /* mpegFile = open("/cd/MPEGAV/MUSIC01.DAT", _READ); */ /* Top Gun*/
-        /*mpegFile = open("/cd/ma", _READ);*/              /* Lost Ride Map A*/
-        mpegFile = open("/cd/RTF/application.rtf", _READ); /* Guignol */
+        mpegFile = open("/cd/ma", _READ); /* Lost Ride Map A*/
     }
     DEBUG(mpegFile >= 0);
 
-    DEBUG(lseek(mpegFile, 0x11DEA000, 0)); /* 299802624 */
+    DEBUG(lseek(mpegFile, 0x03520800, 0));
     DEBUG(ss_play(mpegFile, &mpegPcb));
     printf("Started Play %d\n", mpegFile);
 #endif
@@ -720,7 +719,7 @@ void record_state() {
             regdump[regdump_index][24] = imgrt;
             regdump[regdump_index][25] = picrt;
             regdump[regdump_index][26] = fmv_dclk;
-
+            
             regdump[regdump_index][27] = dclkdiff;
 
             regdump_index++;
